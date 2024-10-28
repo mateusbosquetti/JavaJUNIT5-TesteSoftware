706d0b488b1172fb3abc47003498ce9a15f8a900
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2024-10-28 19:11:21 -0300

    a

---
+++ src/main/java/weg/arquiteturaSoftware/Aula11/LibraryService.java
@@ -0,0 +1,21 @@
+package weg.arquiteturaSoftware.Aula11;
+
+public class LibraryService {
+
+    private BookInventoryService bookInventoryService;
+
+    public LibraryService(BookInventoryService bookInventoryService) {
+        this.bookInventoryService = bookInventoryService;
+    }
+
+    public boolean lendBookToUser(String title) {
+
+        if (bookInventoryService.isBookAvailable(title)) {
+            bookInventoryService.lendBook(title);
+            return true;
+        }
+        return false;
+
+    }
+
+}
